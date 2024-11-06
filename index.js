45bd5349d6854555ffbdbf6e46f6c249679ec477
chiemella <williamschiemelaod@gmail.com>
2024-11-06 10:25:38 +0100

    Use Bracket Notation to Find the First Character in a String

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ Example:
 const ourName = "freeCodeCamp";
 const ourStr = "Hello, our name is " + ourName + ", how are you?";
 ourStr would have a value of the string Hello, our name is freeCodeCamp, how are you?.
-
 */
-
 const myName = "Chiemella,";
 const myStr = "My name is " + myName + " I am well!";
 console.log(myStr)
@@ -27,7 +25,6 @@ const anAdjective = "awesome!";
 let ourStr = "freeCodeCamp is ";
 ourStr += anAdjective;
 ourStr would have the value freeCodeCamp is awesome!.
-
  */
 
 const someAdjective = "nice";
@@ -44,9 +41,7 @@ console.log("Alan Peter".length);
 The value 10 would be displayed in the console. Note that the space character between "Alan" and "Peter" is also counted.
 
 For example, if we created a variable const firstName = "Ada", we could find out how long the string Ada is by using the firstName.length property.
-
  */
-
 let lastNameLength = 0;
 const lastName = "Love";
 
@@ -54,3 +49,26 @@ const lastName = "Love";
 lastNameLength = lastName.length;
 
 console.log(lastNameLength); // Outputs: 4
+
+
+
+/* 
+Use Bracket Notation to Find the First Character in a String
+Bracket notation is a way to get a character at a specific index within a string.
+
+Most modern programming languages, like JavaScript, don't start counting at 1 like humans do. They start at 0. This is referred to as Zero-based indexing.
+
+For example, the character at index 0 in the word Charles is C. So if const firstName = "Charles", you can get the value of the first letter of the string by using firstName[0].
+
+Example:
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+firstLetter would have a value of the string C. 
+*/
+let firstLetterOfLastName = "";
+const lastNameOf = "Lolace";
+
+firstLetterOfLastName = lastNameOf[0];
+console.log(firstLetterOfLastName);
+
