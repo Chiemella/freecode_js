2fe71613999eec91861df129e655a3d88c46864e
chiemella <williamschiemelaod@gmail.com>
2024-11-06 12:48:57 +0100

    Understand String Immutability

--- index.js
+++ index.js
@@ -88,3 +88,8 @@ myStr = "Job"; */
  Mstr = "Hello World";
  console.log(Mstr);
 
+ /*  */
+ const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+console.log(secondLetterOfFirstName);
+
