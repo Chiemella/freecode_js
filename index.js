ee653770857865458af5c03465acdf8edff006a0
chiemella <williamschiemelaod@gmail.com>
2024-11-07 17:45:26 +0100

    Arrays

--- index.js
+++ index.js
@@ -129,3 +129,15 @@ const wordText = "This " + "is " + eyeText + " " + handText + " " + legText;
 console.log(wordText)
 
 
+/* Store Multiple Values in one Variable using JavaScript Arrays
+With JavaScript array variables, we can store several pieces of data in one place.
+
+You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+ */
+
+const myArray = ["hope", 12,];
+console.log(myArray);
+
+
