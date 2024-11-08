4979f014872e8a6ff118f159566f062df224ba5a
chiemella <williamschiemelaod@gmail.com>
2024-11-08 10:40:22 +0100

    Nested Array

--- index.js
+++ index.js
@@ -148,6 +148,24 @@ You can also nest arrays within other arrays, like below:
 const teams = [["Bulls", 23], ["White Sox", 45]];
 This is also called a multi-dimensional array. */
 
-const muArray = [["Hope"], [12]];
-console.log(muArray);
+/* const muArray = [["Hope"], [12]];
+console.log(muArray); */
 
+
+/* Access Array Data with Indexes
+We can access the data inside arrays using indexes.
+
+Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array. Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.
+
+
+Example
+
+const array = [50, 60, 70];
+console.log(array[0]);
+const data = array[1];
+The console.log(array[0]) prints 50, and data has the value 60. */
+
+const muArray = [12, 30, 90];
+const myData = muArray[0];
+
+console.log(myData);
