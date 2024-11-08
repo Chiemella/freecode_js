9f792dd45e39d7b5b998e97219cbcf73209eafad
chiemella <williamschiemelaod@gmail.com>
2024-11-08 10:50:35 +0100

    Modifying Arrays

--- index.js
+++ index.js
@@ -169,3 +169,25 @@ const muArray = [12, 30, 90];
 const myData = muArray[0];
 
 console.log(myData);
+
+
+
+/* Modify Array Data With Indexes
+Unlike strings, the entries of arrays are mutable and can be changed freely, even if the array was declared with const.
+
+Example
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+ourArray now has the value [15, 40, 30].
+
+Note: There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+
+Modify the data stored at index 0 of myArray to a value of 45.
+
+*/
+
+
+const ouArray = [23, 37, 80];
+ouArray[0] = 100;
+console.log(ouArray);
