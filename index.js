0c315fee9924817acb3bd57870833ca637990953
chiemella <williamschiemelaod@gmail.com>
2024-11-08 10:29:17 +0100

    Nested Array

--- index.js
+++ index.js
@@ -141,3 +141,13 @@ const myArray = ["hope", 12,];
 console.log(myArray);
 
 
+
+/* Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array. */
+
+const muArray = [["Hope"], [12]];
+console.log(muArray);
+
