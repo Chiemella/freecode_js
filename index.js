0a567cce3c6af67ff0638a82b4dce35828c7c514
chiemella <williamschiemelaod@gmail.com>
2024-11-10 09:26:05 +0100

    scope

--- index.js
+++ index.js
@@ -331,3 +331,30 @@ console.log(answer2);
 console.log(answer3);
 
 
+
+
+/* 
+This code demonstrates the difference between a globally scoped variable declared with let and a global variable created without let or const inside a function.
+*/
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
